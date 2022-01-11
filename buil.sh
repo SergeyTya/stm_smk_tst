ea46ab15469d64bde94d31fc7ad6374d90c25088
sergey_ubuntu <sergey_ty@mail.ru>
2022-01-11 15:21:08 +0300

    fix Jlink debug

---
+++ buil.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#cmake -G 'Eclipse CDT4 - Unix Makefiles' -DCMAKE_TOOLCHAIN_FILE=arm-gcc-toolchain.cmake -DCMAKE_ECLIPSE_VERSION=4.2 -DCMAKE_BUILD_TYPE=Debug -S . -B../build
+#cmake -G 'Unix Makefiles' -DCMAKE_BUILD_TYPE=Debug -S . -Bbuild
+#cmake --build build
+
+
+cmake -G "Eclipse CDT4 - Unix Makefiles" -DCMAKE_TOOLCHAIN_FILE=arm-gcc-toolchain.cmake -DCMAKE_ECLIPSE_VERSION=4.2 -DCMAKE_BUILD_TYPE=Debug -S . -B../build
